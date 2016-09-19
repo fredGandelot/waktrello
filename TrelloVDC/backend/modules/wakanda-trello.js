cb8dab4f2b5f09bb9c2792e0be18c15975c8c099
ELBOUHASSANI Omar <elbouhassaniomar@gmail.com>
2016-09-19 14:34:27 +0200

    add methods in wakanda-trello

--- TrelloVDC/backend/modules/wakanda-trello.js
+++ TrelloVDC/backend/modules/wakanda-trello.js
@@ -1,4 +1,7 @@
+  
+//GET   (Read)
  
+
 exports.getBoards = function (appkey,token,username) {
 	var request='https://api.trello.com/1/members/'+username+'/boards?lists=open&list_fields=name&fields=name,desc&key='+appkey+'&token='+token ;
 	var xhr=new XMLHttpRequest();
@@ -16,6 +19,7 @@ exports.getListsOfABoard=function(appkey,token,idBoard){
 	return JSON.parse(xhr.responseText);
 }
 
+
 exports.getCardsOfAlist=function(appkey,token,idList){
 	
 	var request='https://api.trello.com/1/lists/'+idList+'/cards?key='+appkey+'&token='+token ;
@@ -32,3 +36,33 @@ exports.getMembersOfABoard=function(appkey,token,idBoard){
     xhr.send();
 	return JSON.parse(xhr.responseText);
 }
+
+
+// POST
+
+
+exports.createNewCard=function(appkey,token,idList,card){	
+ 
+	var request='https://api.trello.com/1/cards?key='+appkey+'&token='+token+'&idList='+idList+'&name='+card.name+'&desc='+card.desc+'&due=';
+	var xhr=new XMLHttpRequest();
+    xhr.open('POST',request,true);
+    xhr.send();
+	return JSON.parse(xhr.responseText)
+ 
+}
+
+// PUT   
+
+exports.renameCard=function(appkey,token,idCard,value){
+	
+
+	var request='https://api.trello.com/1/cards/'+idCard+'/name'+'?key='+appkey+'&token='+token+'&value='+value;
+	var xhr=new XMLHttpRequest();
+    xhr.open('PUT',request,true);
+    xhr.send();
+	return JSON.parse(xhr.responseText)
+	
+}
+
+
+
