b90315f86eb60efe3ad6ce4ba801d55c3709d2bf
ELBOUHASSANI Omar <elbouhassaniomar@gmail.com>
2016-09-20 18:05:53 +0200

    add new features

--- TrelloVDC/test.js
+++ TrelloVDC/test.js
@@ -3,19 +3,13 @@
 //ds.Board.orderBy("name desc,ID desc");
 // bord projet taxi 57a471485ab2c09495b9afab
 
-
-var b = ds.Board.all()[0]
-b.name="renamed from wakanda héé" ;
-b.save();
-   
-
-
-
-
+// tre1  ---> 57a4710c3588442a5ab2278a
+//var wakTrello = require('wakanda-trello')
 //var appkey = "f527fe40338800eec3c64e8548c29924";
 //var token = "347f9c1d1a527bec54bbee6d3e40810e52d3e93c1c1e687bb9c69e501e0ff26c";
-//var username = 'elbouhassaniomar1'
-//var wakTrello = require('wakanda-trello')
+//wakTrello.getBoardByID(appkey,token,'57a471485ab2c09495b9afab')
 
-//wakTrello.renameBoard(appkey,token,'57a471485ab2c09495b9afab',"Omarrooohahahahddd");
-// 
+ ds.Board('57a471485ab2c09495b9afab') ;
+
+
+ 
---
+++ TrelloVDC/testApi.js
@@ -0,0 +1,30 @@
+ 
+//ds.Board.all();
+
+
+//ds.Board.orderBy('name asc');
+
+
+//ds.Board.orderBy('name desc')
+
+
+// ds.Board.orderBy('name asc,ID asc')
+
+
+// var e = new ds.Board();
+// e
+
+
+//var e= new ds.Board();
+//e.name="board from wakanda";
+//e.desc="desc from wakanda";
+//e.save();
+
+
+//var e= ds.Board('57e16b628d8e4655730452e0');
+//e
+
+
+//var e=ds.Board('57e16b628d8e4655730452e0');
+//e.name="borad from wakanda named";
+//e.save();
